17420248d48fc8906f90f9ba6c338bf929acc030
WedSan <andre.alves.silvazesez@gmail.com>
2024-04-01 14:26:28 -0300

    funcionalidade: Adiciona componente Footer ao componente App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/header/Header.tsx";
 import SpringContainer from "./components/PageOne/SpringContainer.tsx";
 import MainPageOne from "./components/PageOne/MainPageOne.tsx";
 import "./App.css"
-
+import Footer from "./components/PageOne/Footer.tsx";
 function App() {
 
   return (
@@ -10,7 +10,7 @@ function App() {
         <Header></Header>
         <SpringContainer></SpringContainer>
         <MainPageOne></MainPageOne>
-
+        <Footer></Footer>
     </>
   )
 }
