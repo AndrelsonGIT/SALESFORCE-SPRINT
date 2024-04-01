f2295adb457db056be82e2883887227828a6146b
WedSan <andre.alves.silvazesez@gmail.com>
2024-04-01 14:13:24 -0300

    refatoração: Ajusta o elemento img do componente

--- src/components/PageOne/SpringContainer.tsx
+++ src/components/PageOne/SpringContainer.tsx
@@ -5,15 +5,12 @@ const SpringContainer = () => {
 
     return (
         <>
-            <img className="salesforceMainMenu" src={salesforceMainMenu} alt="salesforce_main_menu"/>
-            <div>
-                {/*TODO: FAZER O EFEITO DE DIGITAÇÃO AQUI*/}
-                <div className="container_sales_menu">
-                    <h1>A salesforce faz a sua empresa mais eficiente</h1>
-                    <div className="container_div_buttons">
-                        <ButtonSpring nome="Por que a Salesforce"></ButtonSpring>
-                        <ButtonSpring nome="Produtos"></ButtonSpring>
-                    </div>
+            <div className="container_sales_menu">
+                <h1>A salesforce faz a sua empresa mais eficiente</h1>
+                <img className="salesforce_main_menu_img" src={salesforceMainMenu} alt="salesforce_main_menu"/>
+                <div className="container_div_buttons">
+                    <ButtonSpring nome="Por que a Salesforce"></ButtonSpring>
+                    <ButtonSpring nome="Produtos"></ButtonSpring>
                 </div>
             </div>
         </>
