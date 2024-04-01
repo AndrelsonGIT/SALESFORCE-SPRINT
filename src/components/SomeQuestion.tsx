2020c66b6ab90b3489099c8e6d12aef191d53b56
WedSan <andre.alves.silvazesez@gmail.com>
2024-04-01 14:14:25 -0300

    funcionalidade: Cria o componente SomeQuestion

---
+++ src/components/SomeQuestion.tsx
@@ -0,0 +1,13 @@
+import "./SomeQuestion.css"
+const SomeQuestion = ()=>{
+    return(
+        <>
+            <div className="container_some_question">
+                <h2>Você tem dúvidas? Vamos ajudá-lo a encontrar a direção correta.</h2>
+                <button>Entre em contato</button>
+            </div>
+        </>
+    )
+}
+
+export default SomeQuestion;
