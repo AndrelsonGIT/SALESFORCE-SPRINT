552c2244b3872e4afe03688dd6cc008604bc79eb
WedSan <andre.alves.silvazesez@gmail.com>
2024-04-01 14:25:53 -0300

    funcionalidade: Cria o componente Footer

---
+++ src/components/PageOne/Footer.tsx
@@ -0,0 +1,13 @@
+import "./Footer.css"
+const Footer = ()=>{
+    return(
+        <>
+            <footer>
+                <h4>Salesforce <span>&#174;</span></h4>
+                <h5>Entre em contato: 0800 891 1887</h5>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
