baafb35b49738ac241f398fcbc6a937542f6c1c5
WedSan <andre.alves.silvazesez@gmail.com>
2024-04-01 14:16:23 -0300

    refatoração: remove componente não utilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import Header from "./components/header/Header.tsx";
 import SpringContainer from "./components/PageOne/SpringContainer.tsx";
 import MainPageOne from "./components/PageOne/MainPageOne.tsx";
-import GroupCards from "./components/GroupCards.tsx";
 import "./App.css"
-import Feedback from "./components/Feedback.tsx";
-import cardPequenasEmpresas from "./assets/pequenas_empresas_card.webp";
-import Card from "./components/Card.tsx";
+
 function App() {
 
   return (
