c291232493712c3bac70b4468e3d458b0954ca5f
MarcoPrefeito <marcaosilvamarcao5@gmail.com>
2024-04-01 22:21:39 -0300

    conserta alguns bugs

--- src/components/Products/ProductsCards.tsx
+++ src/components/Products/ProductsCards.tsx
@@ -10,13 +10,13 @@ const ProductsCards = ()=>{
                 <div className="main_container_card">
 
                     <div className="card_container card_marketing">
-                        <img id="marketing_cloud_img" className="sales_img" s src="https://images.squarespace-cdn.com/content/v1/5e6cfa89c315535aba12ee9d/1620070273475-4V6FM9CR26S7STAY6X1N/Logo+-+Marketing+Cloud+%281%29.png" alt=""/>
+                        <img id="marketing_cloud_img" className="sales_img" ssrc="https://images.squarespace-cdn.com/content/v1/5e6cfa89c315535aba12ee9d/1620070273475-4V6FM9CR26S7STAY6X1N/Logo+-+Marketing+Cloud+%281%29.png" alt=""/>
                         <h3 >Marketing Cloudd</h3>
                         <button>SAIBA MAIS</button>
                     </div>
 
                     <div className="card_container card_sales">
-                        <img id="sales_cloud_img" className="sales_img" ssrc="https://yt3.googleusercontent.com/ytc/AIdro_lJamH24JE3ERxWIysBtXXKdXlltSj--aOlN4jf=s900-c-k-c0x00ffffff-no-rj" alt=""/>
+                        <img id="sales_cloud_img" className="sales_img" src="https://yt3.googleusercontent.com/ytc/AIdro_lJamH24JE3ERxWIysBtXXKdXlltSj--aOlN4jf=s900-c-k-c0x00ffffff-no-rj" alt=""/>
                         <h3 >Sales Cloud</h3>
                         <button>SAIBA MAIS</button>
                     </div>
